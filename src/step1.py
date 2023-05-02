f83af406050e42b5d247a63a260b24ee4093b567
dev.am.balamurugan <dev.am.balamurugan@gmail.com>
2023-05-01 23:35:49 -0400

    new py repo

---
+++ src/step1.py
@@ -0,0 +1,10 @@
+print("hello world!")
+sum =6+7
+print(sum)
+print("show this in the console")
+sum = 1 + 2 # 3
+product = sum * 2
+print(product)
+from datetime import date
+print(date.today())
+print("Today's date is: " + str(date.today()))
