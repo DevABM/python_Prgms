708393f12a9bd1a605fba1f9b0b8a900e8ec384d
dev.am.balamurugan <dev.am.balamurugan@gmail.com>
2023-06-21 21:22:18 -0400

    flooradd

---
+++ flooradd.py
@@ -0,0 +1,10 @@
+import math
+def addNumbers(a,b):
+    # a and b are float numbers
+    #function should return the truncated value of the sum
+    sum =0
+    sum = math.floor(a+b)
+    return sum
+
+print(addNumbers(67.98765,450.777777))
+ 
